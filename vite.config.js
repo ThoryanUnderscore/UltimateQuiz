9bd56c38215d7d2389429f8bdf7a86c4db10471d
ThoryanUnderscore <thoryandemaria@gmail.com>
2026-01-12 19:03:36 +0100

    Set base path for GitHub deployment
    Added a base path for GitHub repository deployment.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+export default defineConfig({
+  base: '/nom-de-ton-depot-github/', // AJOUTE CETTE LIGNE
+  plugins: [react()],
+})
+
 export default defineConfig({
   plugins: [
     react({
