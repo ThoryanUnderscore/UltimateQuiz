2fbdf58578535229ae21a5e5e247842fceb1269a
ThoryanUnderscore <thoryandemaria@gmail.com>
2026-01-12 19:13:00 +0100

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Remplace 'nom-de-ton-depot' par le nom exact de ton projet sur GitHub
-  base: '/nom-de-ton-depot/', 
+  base: '/UltimateQuiz/', 
   plugins: [react()],
 })
